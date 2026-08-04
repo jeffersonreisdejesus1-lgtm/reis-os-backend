ab1a2c7d5e6ceb15c42e0e62f0688dbb6b9b8d34
Jefferson Reis <jeffersonreisdejesus1@gmail.com>
2026-08-03 22:35:30 -0300

    Corrige tipos do passlib para MyPy

--- app/shared/security/passwords.py
+++ app/shared/security/passwords.py
@@ -1,11 +1,12 @@
+from typing import cast
 from passlib.context import CryptContext
 
 password_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
-    return password_context.hash(password)
+    return cast(str, password_context.hash(password))
 
 
 def verify_password(password: str, hashed_password: str) -> bool:
-    return password_context.verify(password, hashed_password)
+    return cast(bool, password_context.verify(password, hashed_password))
