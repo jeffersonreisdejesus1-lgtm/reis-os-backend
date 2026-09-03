d8e3b7f21cef6b9806b40a7f65d0d6334c6a3577
jeffersonreisdejesus1-lgtm <jeffersonreisdejesus1@gmail.com>
2026-09-03 11:29:53 -0300

    evidence: add C09 acceptance package surface

---
+++ app/c09_acceptance/__init__.py
@@ -0,0 +1,17 @@
+from .manifest import (
+    FROZEN_PI_IDS,
+    PI_ACCEPTANCE_MANIFEST,
+    AcceptanceObservation,
+    AcceptanceSummary,
+    PIAcceptanceEntry,
+    evaluate_acceptance,
+)
+
+__all__ = [
+    "FROZEN_PI_IDS",
+    "PI_ACCEPTANCE_MANIFEST",
+    "AcceptanceObservation",
+    "AcceptanceSummary",
+    "PIAcceptanceEntry",
+    "evaluate_acceptance",
+]
