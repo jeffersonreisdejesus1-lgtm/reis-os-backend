cb61567e60dee92b762d5b67bde525ed06393be1
jeffersonreisdejesus1-lgtm <jeffersonreisdejesus1@gmail.com>
2026-09-04 19:18:35 -0300

    feat(dedala): start expertise registry refactor

---
+++ app/expertise/__init__.py
@@ -0,0 +1,15 @@
+from .registry import (
+    DEDALA_EXPERTISE_MANIFEST,
+    ExpertiseManifest,
+    ExpertLens,
+    RetrievalPlan,
+    build_retrieval_plan,
+)
+
+__all__ = [
+    "DEDALA_EXPERTISE_MANIFEST",
+    "ExpertiseManifest",
+    "ExpertLens",
+    "RetrievalPlan",
+    "build_retrieval_plan",
+]
