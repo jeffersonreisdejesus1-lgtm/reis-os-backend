38e6029e93accfb2fa6972d78f112ea70466c1fa
jeffersonreisdejesus1-lgtm <jeffersonreisdejesus1@gmail.com>
2026-09-07 22:54:59 -0300

    OCSX: expose L1 qualification runtime

---
+++ app/ocsx_l1/__init__.py
@@ -0,0 +1,9 @@
+from .runtime import L1Checkpoint, L1QualificationRuntime, Outcome, StopReason, UNKNOWN
+
+__all__ = [
+    "L1Checkpoint",
+    "L1QualificationRuntime",
+    "Outcome",
+    "StopReason",
+    "UNKNOWN",
+]
