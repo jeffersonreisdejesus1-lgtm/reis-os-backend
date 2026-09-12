62ad69dfd41b54d926fdfdc6491f0724c47b11d1
jeffersonreisdejesus1-lgtm <jeffersonreisdejesus1@gmail.com>
2026-09-12 04:17:54 -0300

    fix(ab10): preserve canonical cognitive validation exports

--- app/cognitive_validation/__init__.py
+++ app/cognitive_validation/__init__.py
@@ -1,15 +1,43 @@
 from .contracts import (
-    AB0_CONCEPTS,
-    AB0_INVARIANTS,
-    ConceptDefinition,
-    EvidenceLedger,
-    EvidenceRecord,
-    ValidationContractError,
-    assert_ab0_contract,
-    record_cognitive_evidence,
+    AB0_REQUIRED_DEFINITIONS,
+    CognitiveClaim,
+    CognitiveValidationContract,
+    canonical_ab0_contract,
 )
-from .causal_memory import CausalMemoryExperiment, CausalMemoryResult
-from .closed_loop import ClosedCognitiveLoop, ClosedLoopResult
+from .closed_loop import (
+    ClosedCognitiveLoop,
+    ClosedLoopTrace,
+    CognitiveDecision,
+    CognitiveObservation,
+    CognitiveState,
+)
+from .causal_memory import (
+    CausalMemoryStore,
+    MemoryAugmentedDecision,
+    MemoryAugmentedDecisionEngine,
+    MemoryRecord,
+)
+from .longitudinal_learning import (
+    ContextualPolicy,
+    LearningCycle,
+    LongitudinalExperimentResult,
+    LongitudinalMetrics,
+    run_longitudinal_learning_experiment,
+)
+from .plasticity import (
+    DriftAdaptivePolicy,
+    FrozenPolicy,
+    PlasticityCycle,
+    PlasticityExperimentResult,
+    PlasticityMetrics,
+    run_plasticity_experiment,
+)
+from .self_regulation import (
+    CognitiveControlState,
+    RegulatoryAction,
+    RegulatoryDecision,
+    SelfRegulationEngine,
+)
 from .distributed_integrated_cognition import (
     CognitiveActor,
     DistributedCognitionResult,
@@ -18,75 +46,40 @@ from .distributed_integrated_cognition import (
     Message,
     OCS_IDS,
 )
-from .failure_resilience import (
-    CognitiveFailureResilienceEngine,
-    FailureDisposition,
-    FaultInjection,
-    FaultKind,
-)
-from .identity_preserving_adaptation import (
-    IdentityPreservingAdaptation,
-    IdentitySnapshot,
-    RecoveryResult,
-)
-from .integrated_architecture import (
-    COMPONENTS,
-    AblationResult,
-    IntegratedCognitiveArchitecture,
-)
-from .longitudinal_learning import LongitudinalLearningExperiment, LongitudinalLearningResult
-from .metacognition import (
-    MetacognitiveRevisionEngine,
-    MetacognitiveRevisionResult,
-    PolicyCandidate,
-)
-from .plasticity import DriftAdaptivePolicy, PlasticityExperiment, PlasticityResult
-from .self_regulation import (
-    CognitiveControlState,
-    RegulatoryAction,
-    SelfRegulationDecision,
-    SelfRegulationEngine,
-)
 
 __all__ = [
-    "AB0_CONCEPTS",
-    "AB0_INVARIANTS",
-    "ConceptDefinition",
-    "EvidenceLedger",
-    "EvidenceRecord",
-    "ValidationContractError",
-    "assert_ab0_contract",
-    "record_cognitive_evidence",
-    "CausalMemoryExperiment",
-    "CausalMemoryResult",
+    "AB0_REQUIRED_DEFINITIONS",
+    "CognitiveClaim",
+    "CognitiveValidationContract",
+    "canonical_ab0_contract",
     "ClosedCognitiveLoop",
-    "ClosedLoopResult",
+    "ClosedLoopTrace",
+    "CognitiveDecision",
+    "CognitiveObservation",
+    "CognitiveState",
+    "CausalMemoryStore",
+    "MemoryAugmentedDecision",
+    "MemoryAugmentedDecisionEngine",
+    "MemoryRecord",
+    "ContextualPolicy",
+    "LearningCycle",
+    "LongitudinalExperimentResult",
+    "LongitudinalMetrics",
+    "run_longitudinal_learning_experiment",
+    "DriftAdaptivePolicy",
+    "FrozenPolicy",
+    "PlasticityCycle",
+    "PlasticityExperimentResult",
+    "PlasticityMetrics",
+    "run_plasticity_experiment",
+    "CognitiveControlState",
+    "RegulatoryAction",
+    "RegulatoryDecision",
+    "SelfRegulationEngine",
     "CognitiveActor",
     "DistributedCognitionResult",
     "DistributedIntegratedCognition",
     "GovernedBus",
     "Message",
     "OCS_IDS",
-    "CognitiveFailureResilienceEngine",
-    "FailureDisposition",
-    "FaultInjection",
-    "FaultKind",
-    "IdentityPreservingAdaptation",
-    "IdentitySnapshot",
-    "RecoveryResult",
-    "COMPONENTS",
-    "AblationResult",
-    "IntegratedCognitiveArchitecture",
-    "LongitudinalLearningExperiment",
-    "LongitudinalLearningResult",
-    "MetacognitiveRevisionEngine",
-    "MetacognitiveRevisionResult",
-    "PolicyCandidate",
-    "DriftAdaptivePolicy",
-    "PlasticityExperiment",
-    "PlasticityResult",
-    "CognitiveControlState",
-    "RegulatoryAction",
-    "SelfRegulationDecision",
-    "SelfRegulationEngine",
 ]
