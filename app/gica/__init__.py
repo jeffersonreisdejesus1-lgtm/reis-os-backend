92ddaf8a3716457bd27d57b71eb879fdf4423e70
jeffersonreisdejesus1-lgtm <jeffersonreisdejesus1@gmail.com>
2026-09-12 20:28:00 -0300

    GICA GA4 slice 001: initialize governed program core

---
+++ app/gica/__init__.py
@@ -0,0 +1,15 @@
+from .contracts import (
+    CANONICAL_OCS_ROSTER,
+    GicaGate,
+    GicaProgramContract,
+    GicaProgramState,
+    ProgramTransitionError,
+)
+
+__all__ = [
+    "CANONICAL_OCS_ROSTER",
+    "GicaGate",
+    "GicaProgramContract",
+    "GicaProgramState",
+    "ProgramTransitionError",
+]
