8d3dfdf2afe6cd360c2f33ee40e61de772fde3dd
jeffersonreisdejesus1-lgtm <jeffersonreisdejesus1@gmail.com>
2026-09-14 20:33:43 -0300

    CUPUWA MVP: bump baseline version to 0.3.0

--- frontend/cupuwa/app/build.gradle.kts
+++ frontend/cupuwa/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jsonsoftware.cupuwa"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.1-caixa"
+        versionCode = 4
+        versionName = "0.3.0-mvp"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
